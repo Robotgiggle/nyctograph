3994307087fe456dd790c10775ca4fa5a270c569
Robotgiggle <88736742+Robotgiggle@users.noreply.github.com>
2026-03-01 23:25:05 -0500

    Add page header, basic dream-entry form, and entry list page

--- main.py
+++ main.py
@@ -7,30 +7,6 @@ from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 from pydantic import BaseModel
 
-# ===== APP SETUP =====
-
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app.add_middleware(SessionMiddleware, secret_key="correcthorsebatterystaple")
-
-def flash_context(request: Request): return {"flash": request.session.pop("flashMessages", [])}
-templates = Jinja2Templates(directory="templates", context_processors=[flash_context])
-
-ph = PasswordHasher()
-
-fake_db_users = {
-    "admin": {
-        "username": "admin",
-        "pwhash": ph.hash("dream"),
-        "number": 1868
-    },
-    "user1": {
-        "username": "user1",
-        "pwhash": ph.hash("pw123"),
-        "number": 57
-    }
-}
-
 # ===== UTILITIES =====
 
 def validate_user(uname: str, pword: str):
@@ -49,12 +25,94 @@ def flash(request: Request, message: str, type: str):
         request.session["flashMessages"] = []
     request.session["flashMessages"].append((message, type))
 
+def global_context(request: Request): 
+    return {
+        "flash": request.session.pop("flashMessages", []),
+        "uname": request.session.get("uname")
+    }
+
+def not_implemented_yet(request: Request):
+    flash(request, "This page or method ("+str(request.url)+") has not been implemented yet!", "warn")
+    return RedirectResponse("/", status_code=303)
+
+# ===== APP SETUP =====
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(SessionMiddleware, secret_key="correcthorsebatterystaple")
+
+templates = Jinja2Templates(directory="templates", context_processors=[global_context])
+
+ph = PasswordHasher()
+
+fake_db_users = {
+    "user1": {
+        "username": "user1",
+        "pwhash": ph.hash("pw123"),
+        "entries": []
+    },
+    "user2": {
+        "username": "user2",
+        "pwhash": ph.hash("pw456"),
+        "entries": []
+    }
+}
+
 # ===== ROUTES =====
 
+# homepage / dream entry form
 @app.get("/")
-def root(request: Request):
+def record_dream_form(request: Request):
+    storedEntry = request.session.get("storedEntry")
+    return templates.TemplateResponse(request, "record_dream.html", {"entry": storedEntry})
+
+@app.post("/")
+def record_dream_action(request: Request, title: Annotated[str, Form()], description: Annotated[str, Form()]):
     user = get_user(request)
-    return templates.TemplateResponse(request, "root.html", {"user": user})
+    if user:
+        user["entries"].append((title, description))
+        flash(request, "Dream entry saved", "info")
+        return RedirectResponse("/", status_code=303)
+    else:
+        request.session["storedEntry"] = (title, description)
+        flash(request, "Dream entry temporarily stored - create an account to save it!", "info")
+        return RedirectResponse("/signup", status_code=303)
+    
+# view stored entries
+
+@app.get("/my-dreams")
+def list_dream_entries(request: Request):
+    user = get_user(request)
+    if not user:
+        flash(request, "This page or method requires an account!", "warn")
+        return RedirectResponse("/login", status_code=303)
+    entries = user["entries"]
+    print(entries)
+    return templates.TemplateResponse(request, "my-dreams.html", {"entries": entries})
+
+# view global stats
+
+@app.get("/global-stats")
+def view_global_stats(request: Request):
+    return not_implemented_yet(request)
+
+# general info page
+
+@app.get("/about-us")
+def about_us(request: Request):
+    return not_implemented_yet(request)
+
+# account creation
+
+@app.get("/signup")
+def signup_form(request: Request):
+    return not_implemented_yet(request)
+
+@app.post("/signup")
+def signup_action(request: Request): 
+    return not_implemented_yet(request)
+
+# login system
 
 @app.get("/login")
 def login_form(request: Request):
