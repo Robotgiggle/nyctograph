59c224e196277cb3109f356b2e97cc075ca07b96
Robotgiggle <88736742+Robotgiggle@users.noreply.github.com>
2026-03-30 22:20:43 -0400

    Remove tag id field, use value as PK

--- app/models/tag.py
+++ app/models/tag.py
@@ -12,17 +12,16 @@ entry_tag_association_table = Table(
     "dream_entry_tags",
     Base.metadata,
     Column("entry_id", ForeignKey("dream_entries.id"), primary_key=True),
-    Column("tag_id", ForeignKey("tags.id"), primary_key=True),
+    Column("tag_val", ForeignKey("tags.value"), primary_key=True),
 )
 
 class Tag(Base):
     __tablename__ = "tags"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    value: Mapped[str] = mapped_column(primary_key=True)
     category: Mapped[str]
-    value: Mapped[str]
 
     entries: Mapped[List["DreamEntry"]] = relationship(back_populates="tags", secondary=entry_tag_association_table)
 
     def __repr__(self) -> str:
-        return f"Tag(id: {self.id}, cat: {self.category}, val: {self.value})"
+        return f"Tag(val: {self.value}, cat: {self.category})"
--- app/models/tag_association.py
+++ app/models/tag_association.py
@@ -10,12 +10,12 @@ if TYPE_CHECKING:
 class TagAssociation(Base):
     __tablename__ = "tag_associations"
 
-    tag_id_a: Mapped[int] = mapped_column(ForeignKey("tags.id"), primary_key=True)
-    tag_id_b: Mapped[int] = mapped_column(ForeignKey("tags.id"), primary_key=True)
+    tag_val_a: Mapped[str] = mapped_column(ForeignKey("tags.value"), primary_key=True)
+    tag_val_b: Mapped[str] = mapped_column(ForeignKey("tags.value"), primary_key=True)
     association_day: Mapped[float] = mapped_column(index=True)
     association_week: Mapped[float] = mapped_column(index=True)
     association_month: Mapped[float] = mapped_column(index=True)
     association_all: Mapped[float] = mapped_column(index=True)
 
-    tag_a: Mapped["Tag"] = relationship(foreign_keys=[tag_id_a])
-    tag_b: Mapped["Tag"] = relationship(foreign_keys=[tag_id_b])
+    tag_a: Mapped["Tag"] = relationship(foreign_keys=[tag_val_a])
+    tag_b: Mapped["Tag"] = relationship(foreign_keys=[tag_val_b])
