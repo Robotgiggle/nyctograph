cf2accf2c291190c9b76be6b956cd0508ea582fe
Robotgiggle <88736742+Robotgiggle@users.noreply.github.com>
2026-03-31 19:46:51 -0400

    Streamline validation error response

--- app/main.py
+++ app/main.py
@@ -36,6 +36,5 @@ app.include_router(misc.router)
 @app.exception_handler(RequestValidationError)
 def validation_error_handler(request: Request, exc: RequestValidationError):
     for error in exc.errors():
-        field = error["loc"][1] if len(error["loc"]) > 1 else error["loc"][0]
-        flash(request, f"Validation error for '{field}': {error['msg']}", "warn")
+        flash(request, f"Validation error for '{error['loc'][-1]}': {error['msg']}", "warn")
     return RedirectResponse(request['path'], status_code=303)
