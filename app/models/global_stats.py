1bd2869b2c746a362b400e89d89441786b81cfef
Robotgiggle <88736742+Robotgiggle@users.noreply.github.com>
2026-04-02 19:33:30 -0400

    Add age bracket to global stats, rework tag associations

--- app/models/global_stats.py
+++ app/models/global_stats.py
@@ -1,18 +1,24 @@
-from sqlalchemy import CheckConstraint
+from typing import TYPE_CHECKING
+from sqlalchemy import CheckConstraint, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..database import Base
 
-# There will only ever be four of these, one with each time_slice value
-# Whenever the global stats are recalculate, they'll be replaced
+if TYPE_CHECKING:
+    from . import TagAssociation
+
+# There will only ever be sixteen of these, one with each combination of time_slice and age_bracket
+# Whenever the global stats are recalculated, they'll be replaced
 
 class GlobalStats(Base):
     __tablename__ = "global_stats"
     __table_args__ = (
         CheckConstraint("time_slice IN ('day', 'week', 'month', 'all')"),
+        CheckConstraint("age_bracket IN ('13-29', '30-49', '50+', 'all')"),
     )
 
     time_slice: Mapped[str] = mapped_column(primary_key=True)
+    age_bracket: Mapped[str] = mapped_column(primary_key=True)
     total_entries: Mapped[int]
     top_content_tag: Mapped[str]
     second_content_tag: Mapped[str]
@@ -26,4 +32,7 @@ class GlobalStats(Base):
     smell_rate: Mapped[float]
     taste_rate: Mapped[float]
     pain_rate: Mapped[float]
+    other_rate: Mapped[float]
     avg_sleep_duration: Mapped[float]
+
+    tag_associations: Mapped['TagAssociation'] = relationship(back_populates="stats_obj")
--- app/models/tag_association.py
+++ app/models/tag_association.py
@@ -1,21 +1,27 @@
 from typing import TYPE_CHECKING
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, ForeignKeyConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..database import Base
 
 if TYPE_CHECKING:
-    from . import Tag
+    from . import Tag, GlobalStats
 
 class TagAssociation(Base):
     __tablename__ = "tag_associations"
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ["time_slice","age_bracket"],
+            ["global_stats.time_slice","global_stats.age_bracket"]
+        ),
+    )
 
     tag_val_a: Mapped[str] = mapped_column(ForeignKey("tags.value"), primary_key=True)
     tag_val_b: Mapped[str] = mapped_column(ForeignKey("tags.value"), primary_key=True)
-    association_day: Mapped[float] = mapped_column(index=True)
-    association_week: Mapped[float] = mapped_column(index=True)
-    association_month: Mapped[float] = mapped_column(index=True)
-    association_all: Mapped[float] = mapped_column(index=True)
+    time_slice: Mapped[str]
+    age_bracket: Mapped[str]
+    association_rate: Mapped[float]
 
     tag_a: Mapped["Tag"] = relationship(foreign_keys=[tag_val_a])
     tag_b: Mapped["Tag"] = relationship(foreign_keys=[tag_val_b])
+    stats_obj: Mapped["GlobalStats"] = relationship(back_populates="tag_associations")
